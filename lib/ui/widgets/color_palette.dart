dd1cbc2a8fa92403ca142d09cb778c46c32ca97f
Juan David Julio San Juan <61607858+djulioj@users.noreply.github.com>
2023-02-07 09:16:58 -0500

    Palette color, files and columns

--- lib/ui/widgets/color_palette.dart
+++ lib/ui/widgets/color_palette.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 import '../../utils/color_utils.dart';
 
 class ColorPalette extends StatelessWidget {
-  const ColorPalette({
-    Key? key,
-    // así se reciben en el constructor, por ejemplo
-    //required this.baseColor,
-  }) : super(key: key);
+  const ColorPalette(
+      {Key? key,
+      required this.baseColor1,
+      required this.baseColor2,
+      required this.baseColor3,
+      required this.baseColor4,
+      required this.callback
+      // así se reciben en el constructor, por ejemplo
+      //required this.baseColor,
+      })
+      : super(key: key);
+
+  final String baseColor1;
+  final String baseColor2;
+  final String baseColor3;
+  final String baseColor4;
+  final Function(String) callback;
 
   // atributos de la clase, por ejemplo
   //final String baseColor;
@@ -19,10 +31,47 @@ class ColorPalette extends StatelessWidget {
   Widget build(BuildContext context) {
     // padding para crear espacio alrededor del widget
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      // vamos a mostrar los elementos en un row
-      child: Text(
-          'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),
-    );
+        padding: const EdgeInsets.all(8.0),
+        // vamos a mostrar los elementos en un row
+        child: Row(
+          children: [
+            Expanded(
+              flex: 4,
+              child: GestureDetector(
+                child: Container(
+                  height: 100,
+                  color: ColorUtils.FromHex(baseColor1),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: GestureDetector(
+                child: Container(
+                  height: 100,
+                  color: ColorUtils.FromHex(baseColor2),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: GestureDetector(
+                child: Container(
+                  height: 100,
+                  color: ColorUtils.FromHex(baseColor3),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: GestureDetector(
+                child: Container(
+                  height: 100,
+                  color: ColorUtils.FromHex(baseColor4),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
